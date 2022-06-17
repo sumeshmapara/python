19975967fd34f1411a7c99e9f8f626d0affe2354
Santosh Mapara <sumesh.santosh-mapara@capgemini.com>
2022-06-17 19:52:54 +0530

    upload file

---
+++ Demo.py
@@ -0,0 +1,9 @@
+a=5
+b=6
+
+temp=a
+a=b
+b=temp
+
+print(a)
+print(b)
---
+++ Factorial.py
@@ -0,0 +1,10 @@
+import math
+
+def factorial(n):
+	return(math.factorial(n))
+
+
+
+num = 5
+print("Factorial of", num, "is",
+	factorial(num))
